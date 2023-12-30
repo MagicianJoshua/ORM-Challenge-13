bd9c8e9d45805400608f7705b552c34d0f776679
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2023-12-30 17:09:50 -0400

    added a readme to the file containing the video link

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const data = await Product.findAll({include:[Category, Tag]} )
+  return res.json(data);
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const data = await Product.findByPk(req.params.id, {include:[Category, Tag]} )
+  return res.json(data);
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -92,8 +96,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const data = await Product.findByPk(req.params.id)
+  if (!data) {
+    return res.status(404).json({message:"ID Not found"})
+  }
+  data.destroy();
+  return res.json("Product deleted");
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,50 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const data = await Tag.findAll({include:Product} )
+  return res.json(data);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const data = await Tag.findByPk(req.params.id, {include:Product} )
+  return res.json(data);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  const Data = await Tag.create(req.body); 
+if (!Data) {
+  return res.status(404).json({ message: 'formatting incorrect' });
+}
+return res.json(Data)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+
+  const data = await Tag.findByPk(req.params.id);
+
+  if (data) {
+    await data.update(req.body);
+    res.send({message:"Tag updated successfully"})
+  }else {
+    res.send({message:"Could not find Tag id"});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  const data = await Tag.findByPk(req.params.id)
+  if (!data) {
+    return res.status(404).json({message:"ID Not found"})
+  }
+  data.destroy();
+  return res.json("Tag deleted");
 });
 
 module.exports = router;
